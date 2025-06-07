994c932e21fb49ba4bea58b97430e3d89b576a80
Phuong Khanh Pham <phuong-khanh.pham@epita.fr>
2025-06-07 06:00:51 +0200

    step 6: add cron_job.py

---
+++ src/fit/cron_job.py
@@ -0,0 +1,38 @@
+import requests
+import os
+
+API_BASE_URL = os.getenv("MONOLITH_URL", "http://localhost:12101")
+USER_EMAIL = os.getenv("USER_EMAIL", "jane.doe@mail.com")
+USER_PASS = os.getenv("USER_PASS", "1(#mj0|KyC")
+
+
+# get token by logging in with user credentials
+def get_token():
+    """
+    Get the API token for the user.
+    """
+    url = f"{API_BASE_URL}/oauth/token"
+    response = requests.post(url, json={"email": USER_EMAIL, "password": USER_PASS})
+    
+    if response.status_code == 200:
+        return response.json().get("access_token")
+    else:
+        raise RuntimeError(f"Failed to get token: {response.status_code} - {response.text}")
+    
+    
+# call the create WOD endpoint for all users with token
+def call_create_wod_for_all_users(token):
+    url = f"{API_BASE_URL}/users/wod_for_all"
+    headers = {"Authorization": f"Bearer {token}"}
+    response = requests.post(url, headers=headers)
+    if response.status_code == 200:
+        print("WOD created for all users successfully.")
+    else:
+        print(f"Failed to create WOD: {response.status_code} - {response.text}")
+
+if __name__ == "__main__":
+    try:
+        token = get_token()
+        call_create_wod_for_all_users(token)
+    except Exception as e:
+        print(f"Error: {e}")
