102de97ea3541b5af3e1ec1745c171aff3867e27
natthanicha <natthanicha.vongjarit@epita.fr>
2025-06-17 21:25:02 +0200

    fix: notification service problems fixed

--- src/notification/app.py
+++ src/notification/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from threading import Thread
 from queue_consumer import start_consumer
 from scheduler import check_and_notify
+from service import notifications_store
 
 app = Flask(__name__)
 
@@ -13,6 +14,11 @@ def home():
 def run_check():
     check_and_notify()
     return "Check ran", 200
+
+@app.route('/notifications/<user_id>')
+def get_notifications(user_id):
+    notifications = notifications_store.get(user_id, [])
+    return {"notifications": notifications}, 200
 
 def start_consumer_thread():
     consumer_thread = Thread(target=start_consumer)
--- src/notification/service.py
+++ src/notification/service.py
@@ -1,2 +1,7 @@
+notifications_store = {}
+
 def send_notification(user_id, message):
     print(f"[NOTIFY] To user {user_id}: {message}")
+    if user_id not in notifications_store:
+        notifications_store[user_id] = []
+    notifications_store[user_id].append(message)
